6169baf8da6714bf87349822e87a1c472905a828
Alan Cao <alannesta@sina.com>
2019-03-21 22:05:04 -0400

    use resolver for resolving videos.

--- src/91crawler.js
+++ src/91crawler.js
@@ -1,4 +1,3 @@
-const parserService = require('./service/parser-service');
 const parserServiceV2 = require('./service/parser-service-v2');
 
 const logger = require('./utils/logger');
@@ -8,18 +7,6 @@ const http = require('http');
 const server = http.createServer((req, res) => {
 	var parsed = require('url').parse(req.url);
 	if (parsed.pathname === '/parse' && parsed.query.indexOf('http') > -1) {
-		//let process = parserService.spawnPhantomProcess(parsed.query);
-		//parserService.resolveVideoUrl(process, {}, (err, result) => {
-		//	if (err) {
-		//		// report error
-		//		res.writeHead(500, { 'Content-Type': 'text/plain' });
-		//		res.end('failed');
-		//	} else {
-		//		logger.debug('Parse success, publish results: ', result);
-		//		res.writeHead(200, { 'Content-Type': 'text/plain' });
-		//		res.end(result);
-		//	}
-		//});
 		parserServiceV2.parse(parsed.query).then((result) => {
 			if(result && result.indexOf('http') > -1) {
 				logger.debug('Parse success, publish results: ', result);
@@ -39,5 +26,7 @@ const server = http.createServer((req, res) => {
 	}
 });
 
-server.listen(process.env.PORT || 8001);
+const port = process.env.PORT || 8001;
+console.log('start server at ' + port);
+server.listen(port);
 
--- src/service/parser-service-v2.js
+++ src/service/parser-service-v2.js
@@ -1,25 +1,8 @@
-const request = require('request');
-const utils = require('../utils/service-util');
+const resolver = require('91video-resolver');
 
 const Parser = {
 	parse: function(url) {
-		return new Promise((resolve, reject) => {
-			let j = request.jar();
-			let cookie = request.cookie('language=cn_CN');
-			j.setCookie(cookie, url);
-
-			request({
-				url: url,
-				jar: j
-			}, (error, response, body) => {
-				if (error) {
-					reject(error);
-				}
-				if (!error && response.statusCode === 200) {
-					resolve(utils.extractHtml5Video(body));
-				}
-			});
-		});
+		return resolver.parse(url);
 	}
 };
 
